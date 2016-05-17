598f0b226c17854a022580127a7dc5329db59371
WithoutHaste <wohaste@gmail.com>
2016-05-17 16:46:35 -0500

    finished custom layout demo
    added images, added text, added text line breaking

--- Mockup/MockupCustomLayout.cs
+++ Mockup/MockupCustomLayout.cs
@@ -18,8 +18,9 @@ public static class CustomLayoutDemo
 	private static Point? mouseDownPoint;
 	private static Point? mouseDragPoint;
 
-	private static List<Rectangle> boxes;
-	private static Rectangle selectedBox;
+	private static List<Spire.Box> boxes;
+	private static Spire.Box hoverBox;
+	private static Spire.Box selectedBox;
 	private static Point? selectedCorner;
 	private static Point? oppositeSelectedCorner;
 	private static Side? selectedSide;
@@ -31,7 +32,21 @@ public static class CustomLayoutDemo
 			RemovePanel();
 		}
 		drawPanel = panel;
-		boxes = new List<Rectangle>();
+		boxes = new List<Spire.Box>();
+		boxes.Add(new Spire.ImageBox() {
+			X = 200
+			, Y = 100
+			, Width = 365
+			, Height = 240
+			, Filename = "fireengine.gif"
+		});
+		boxes.Add(new Spire.ImageBox() {
+			X = 350
+			, Y = 370
+			, Width = 134
+			, Height = 153
+			, Filename = "geisslertubes.gif"
+		});
 		ApplyMode(Mode.AddBox);
 	}
 	
@@ -88,8 +103,10 @@ public static class CustomLayoutDemo
 			ApplyMode(Mode.ResizeBox);
 			ResizeMouseDown(sender, e);
 		}
-		else if(CheckOverSides(e.X, e.Y))
+		else if(CheckOverSides(e.X, e.Y) || CheckHoverBox(e.X, e.Y))
 		{
+			if(hoverBox != null)
+				selectedBox = hoverBox;
 			ApplyMode(Mode.MoveBox);
 			MoveMouseDown(sender, e);
 		}
@@ -101,12 +118,26 @@ public static class CustomLayoutDemo
 	
 	private static void AddMouseUp(object sender, MouseEventArgs e)
 	{
-		boxes.Add(new Rectangle(
-			Math.Min(mouseDownPoint.Value.X, e.X)
-			, Math.Min(mouseDownPoint.Value.Y, e.Y)
-			, Math.Abs(mouseDownPoint.Value.X - e.X)
-			, Math.Abs(mouseDownPoint.Value.Y - e.Y)
-		));
+		string text = "";
+		switch(boxes.Count)
+		{
+			case 2: text = "4. Logarithmic Casing\n5: Phase Plate"; break;
+			case 3: text = "19. Lodusodelta Plate\nEvery seventh conductor being connected by a non-reversible turmy pipe to the differential girdle spring"; break;
+			case 4: text = "16-18, 22. Transmission\nSupplies inverse reactive current and automatically synchronizes the caridnal grameters"; break;
+			case 5: text = "10. Turbo-encabulator"; break;
+			case 6: text = "12. Drawn Reciprocating Dingle Arm"; break;
+			case 7: text = "7-9, 14. Girdle Spring\nProduces power by the modial interaction of magneto reluctance and capacitive directance"; break;
+			case 8: text = "13. Panametric Fan\nPlaced in-line with the spurving bearings such that side fumbling is minimized"; break;
+			case 9: text = "16. Unilateral Phase Detractors"; break;
+			case 10: text = "- Charmed\n- Deelated\n- Elucidated\n- Spivy\n- Marzine\n- Hydrocoptic"; break;
+		}
+		boxes.Add(new Spire.TextBox() {
+			X = Math.Min(mouseDownPoint.Value.X, e.X)
+			, Y = Math.Min(mouseDownPoint.Value.Y, e.Y)
+			, Width = Math.Abs(mouseDownPoint.Value.X - e.X)
+			, Height = Math.Abs(mouseDownPoint.Value.Y - e.Y)
+			, Text = text
+		});
 		mouseDownPoint = null;
 		drawPanel.Invalidate();
 	}
@@ -126,7 +157,7 @@ public static class CustomLayoutDemo
 				else if(selectedCorner.Value.X == selectedBox.X+selectedBox.Width && selectedCorner.Value.Y == selectedBox.Y+selectedBox.Height)
 					drawPanel.Cursor = Cursors.SizeNWSE;
 			}
-			else if(CheckOverSides(e.X, e.Y))
+			else if(CheckOverSides(e.X, e.Y) || CheckHoverBox(e.X, e.Y))
 			{
 				drawPanel.Cursor = Cursors.Hand;
 			}
@@ -187,7 +218,7 @@ public static class CustomLayoutDemo
 	{
 		selectedBox = null;
 		selectedSide = null;
-		foreach(Rectangle box in boxes)
+		foreach(Spire.Box box in boxes)
 		{
 			if(CheckOverSideHorizontal(box.X, box.Y, box.X+box.Width, box.Y, x, y))
 			{
@@ -221,7 +252,7 @@ public static class CustomLayoutDemo
 		selectedBox = null;
 		selectedCorner = null;
 		oppositeSelectedCorner = null;
-		foreach(Rectangle box in boxes)
+		foreach(Spire.Box box in boxes)
 		{
 			if(CheckOverCorner(box.X, box.Y, x, y))
 			{
@@ -253,6 +284,20 @@ public static class CustomLayoutDemo
 		return (selectedBox != null);
 	}
 	
+	private static bool CheckHoverBox(int x, int y)
+	{
+		hoverBox = null;
+		foreach(Spire.Box box in boxes)
+		{
+			if(x >= box.X && x <= box.X+box.Width && y >= box.Y && y <= box.Y+box.Height)
+			{
+				hoverBox = box;
+				return true;
+			}
+		}
+		return false;
+	}
+	
 	private static bool CheckOverCorner(int cornerX, int cornerY, int x, int y)
 	{
 		int margin = 5;
@@ -278,6 +323,13 @@ public static class CustomLayoutDemo
 		g.Clear(Color.LightGray);
 		//g.SmoothingMode = SmoothingMode.AntiAlias;
 		PaintPage(g, new Rectangle(15,15,800-30,550-30));
+		foreach(Spire.Box box in boxes)
+		{
+			if(box is Spire.ImageBox)
+				PaintImage(g, box as Spire.ImageBox);
+			else if(box is Spire.TextBox)
+				PaintTextBox(g, box as Spire.TextBox);
+		}
 		if(mode == Mode.AddBox)
 		{
 			if(mouseDownPoint != null && mouseDragPoint != null)
@@ -301,23 +353,22 @@ public static class CustomLayoutDemo
 				PaintDragBox(g, selectedBox.X+deltaX, selectedBox.Y+deltaY, selectedBox.Width, selectedBox.Height);
 			}
 		}
-		foreach(Rectangle box in boxes)
-		{
-			PaintTextBox(g, box);
-		}
-		PaintImage(g, new Rectangle(300,200,365,240), "fireengine.gif");
-		PaintImage(g, new Rectangle(50,100,134,153), "geisslertubes.gif");
 		g.Dispose();
 		pea.Graphics.DrawImageUnscaled(graphicsBuffer, 0, 0);
 	}
 
-	private static void PaintImage(Graphics g, Rectangle rect, string filename)
+	private static void PaintImage(Graphics g, Spire.ImageBox box)
 	{
-		if(!File.Exists(filename)) return;
+		if(!File.Exists(box.Filename)) return;
 			
-		using (Image src = Image.FromFile(filename))
+		using (Image src = Image.FromFile(box.Filename))
 		{
-			g.DrawImage(src, rect.X, rect.Y, rect.Width, rect.Height);
+			g.DrawImage(src, box.X, box.Y, box.Width, box.Height);
+			if(boxes.Count < 11)
+			{
+				Pen pen = new Pen(Color.Gray, 1);
+				g.DrawRectangle(pen, box.X, box.Y, box.Width, box.Height);
+			}
 		}
 	}
 
@@ -341,9 +392,90 @@ public static class CustomLayoutDemo
 		g.DrawRectangle(pen, Math.Min(a.X,b.X), Math.Min(a.Y,b.Y), Math.Abs(a.X-b.X), Math.Abs(a.Y-b.Y));
 	}
 	
-	private static void PaintTextBox(Graphics g, Rectangle rect)
+	private static void PaintTextBox(Graphics g, Spire.TextBox box)
 	{
-		Pen pen = new Pen(Color.DarkGray, 1);
-		g.DrawRectangle(pen, rect.X, rect.Y, rect.Width, rect.Height);
+		if(boxes.Count < 11)
+		{
+			Pen pen = new Pen(Color.Gray, 1);
+			g.DrawRectangle(pen, box.X, box.Y, box.Width, box.Height);
+		}
+		else
+		{
+			Brush brush = new SolidBrush(Color.Black);
+			Font font = new Font("Times New Roman", 13);
+			int lineHeight = (int)(g.MeasureString("TEST", font).Height);
+			int y = box.Y;
+			int maxWidth = 0;
+			foreach(string line in box.GetLines())
+			{
+				g.DrawString(line, font, brush, box.X, y);
+				y += lineHeight;
+				maxWidth = Math.Max(maxWidth, (int)(g.MeasureString(line, font).Width));
+			}
+			box.Width = maxWidth + 5;
+			box.Height = y + 3 - box.Y;
+		}
+	}
+}
+
+namespace Spire
+{
+	public abstract class Box
+	{
+		public int X;
+		public int Y;
+		public int Width;
+		public int Height;
+	}
+
+	public class ImageBox : Box
+	{
+		public string Filename;
+	}
+
+	public class TextBox : Box
+	{
+		public string Text;
+		private List<string> lines;
+		
+		public List<string> GetLines()
+		{
+			if(lines == null)
+				lines = SplitLines(Text);
+			return lines;
+		}
+	
+		private List<string> SplitLines(string text)
+		{
+			int minLength = 15;
+			int maxLength = 25;
+			List<string> lines = new List<string>(text.Split('\n'));
+			for(int i=1; i<lines.Count; i++)
+			{
+				if(lines[i].Length <= maxLength) continue;
+				string[] words = lines[i].Split(' ');
+				string newLine = "";
+				for(int j=0; j<words.Length; j++)
+				{
+					if(newLine.Length < minLength)
+						newLine += " " + words[j];
+					else if(newLine.Length + words[j].Length <= maxLength)
+						newLine += " " + words[j];
+					else
+					{
+						lines.Insert(i, newLine);
+						i++;
+						newLine = "" + words[j];
+					}
+				}
+				if(newLine != "")
+				{
+					lines.Insert(i, newLine);
+					i++;
+				}
+				lines.RemoveAt(i);
+			}
+			return lines;
+		}
 	}
 }
