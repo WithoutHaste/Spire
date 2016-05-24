b15931f2e9abe76c85c4e31853456c6a277c9150
WithoutHaste <wohaste@gmail.com>
2016-05-24 15:41:43 -0500

    dev data annotations demo

--- Mockup/buildWindows.bat
+++ Mockup/buildWindows.bat
@@ -1,1 +1,1 @@
-csc -out:SpireMockup_Windows.exe SpireMockup.cs MockupGraphs.cs MockupStyles.cs MockupStylesAdvanced.cs MockupFootnotes.cs MockupFormulas.cs MockupPDF.cs MockupTextLayout.cs MockupCustomLayout.cs MockupDataGraphics.cs DoubleBufferedPanel.cs EasyLayout.cs
+csc -out:SpireMockup_Windows.exe SpireMockup.cs MockupGraphs.cs MockupStyles.cs MockupStylesAdvanced.cs MockupFootnotes.cs MockupFormulas.cs MockupPDF.cs MockupTextLayout.cs MockupCustomLayout.cs MockupDataGraphics.cs MockupDataAnnotations.cs DoubleBufferedPanel.cs EasyLayout.cs
