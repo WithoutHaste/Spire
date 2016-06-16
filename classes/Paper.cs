f75ad2b01452a310facb849fb151bc4d815df7ae
WithoutHaste <wohaste@gmail.com>
2016-06-15 20:44:32 -0500

    blinking caret when focused

--- classes/Paper.cs
+++ classes/Paper.cs
@@ -5,15 +5,21 @@ using System.Drawing;
 //using System.Drawing.Drawing2D;
 using System.IO;
 //using System.Text;
+using System.Timers;
 using System.Windows.Forms;
 
 namespace Spire
 {
-	public class Paper : Panel
+	public class Paper : Control
 	{
+		private System.Timers.Timer caretTimer;
+		private bool caretOn = false;
+		
 		public Paper()
 		{
 			SetupDoubleBuffer();
+			this.GotFocus += new EventHandler(PaperGotFocus);
+			this.LostFocus += new EventHandler(PaperLostFocus);
 		}
 		
 		private void SetupDoubleBuffer()
@@ -21,6 +27,27 @@ namespace Spire
 			// Set the value of the double-buffering style bits to true.
 			this.SetStyle(ControlStyles.DoubleBuffer | ControlStyles.UserPaint | ControlStyles.AllPaintingInWmPaint, true);
 			this.UpdateStyles();
+		}
+		
+		private void PaperGotFocus(object sender, EventArgs e)
+		{
+			caretTimer = new System.Timers.Timer(450/*1000=1second*/);
+			caretTimer.Elapsed += new ElapsedEventHandler(CaretTimerElapsed);
+			caretTimer.Enabled = true;
+		}
+		
+		private void PaperLostFocus(object sender, EventArgs e)
+		{
+			if(caretTimer != null)
+			{
+				caretTimer.Enabled = false;
+			}
+		}
+		
+		private void CaretTimerElapsed(object sender, ElapsedEventArgs e)
+		{
+			caretOn = !caretOn;
+			this.Invalidate();
 		}
 
 		protected override void OnPaint(PaintEventArgs e)
@@ -38,6 +65,17 @@ namespace Spire
 		private void OnPaint(Graphics graphics)
 		{
 			graphics.Clear(Color.White);
+			
+			if(this.Focused && caretOn)
+			{
+				DrawCaret(graphics);
+			}
+		}
+		
+		private void DrawCaret(Graphics graphics)
+		{
+			Pen pen = new Pen(Color.Black, 0.75f);
+			graphics.DrawLine(pen, 10, 20, 10, 30);
 		}
 	}
 }
