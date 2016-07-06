6f3593259a2dddd2d45845b70c0d260cd62fc02c
WithoutHaste <wohaste@gmail.com>
2016-07-06 12:54:18 -0500

    minor fixes for Linux build (note that Linux does not show text jiggle problem while typing)

--- classes/DisplayArea.cs
+++ classes/DisplayArea.cs
@@ -66,7 +66,7 @@ namespace Spire
 		
 		public override string ToString()
 		{
-			return "LineBreaks: " + String.Join(", ", _lineBreaks.ToArray());
+			return "LineBreaks: " + String.Join(", ", _lineBreaks.Select(p=>p.ToString()).ToArray());
 		}
 	}
 }
